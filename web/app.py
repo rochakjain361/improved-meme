f00d81186977d4a868031221fb4c43faf2cebfc6
Rochak Jain <rochak_r@ec.iitr.ac.in>
2020-08-04 13:02:23 +0530

    >Restful API for Service Provider on Flask

--- web/app.py
+++ web/app.py
@@ -1,3 +1,156 @@
+"""
+Registration of a user
+Each user gets 10 tokens
+Store a sentence on our database for 1 token 
+Retrieve his stored sentence on out database for 1 token
+"""
+from flask import Flask, jsonify, request
+from flask_restful  import Api, Resource 
+import os
+import bcrypt
+
+from pymongo import MongoClient
+
+app = Flask(__name__) 
+api = Api(app)
+
+client = MongoClient("mongodb://db:27017")
+db = client.SentencesDatabase
+users = db["Users"]
+
+class Register(Resource):
+    def  post(self):
+        #Step 1 is to get posted data  by the user 
+        postedData = request.get_json()
+
+        #Get the the data
+        username = postedData["username"]
+        password = postedData["password"]
+
+        #hash(password + salt) = dljbgnhbrtbgnernj
+        hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+
+        users.insert_one({
+            "Username": username,
+            "Password": hashed_pw,
+            "Sentence": "",
+            "Tokens": 10
+        })
+
+        retJson = {
+            "status" : 200,
+            "msg": "You successfully signed up for the API"
+        }
+        return jsonify(retJson)
+
+def verifyPw(username, password):
+    hashed_pw = users.find({
+        "Username":username  
+    })[0]["Password"]
+
+    if bcrypt.hashpw(password.encode('utf8'), hashed_pw) == hashed_pw:
+        return True
+    else:
+        return False
+
+def countTokens(username):
+    tokens = users.find({
+        "Username": username
+    })[0]["Tokens"]
+
+    return tokens
+
+class Store(Resource):
+    def post(self):
+        #Step 1 is to get posted data  by the user 
+        postedData = request.get_json()        
+
+        #Step 2 is to read the data
+        username = postedData["username"]
+        password = postedData["password"]
+        sentence = postedData["sentence"]
+
+        #Step 3 verify the username pw match
+        correct_pw = verifyPw(username, password)
+        if not correct_pw:
+            retJson = {
+                "status" : 302
+            }
+            return jsonify(retJson)
+
+        #Step 4 verify the tokens
+        num_tokens = countTokens(username)
+
+        if num_tokens <= 0:
+            retJson = {
+                "status" : 301
+            }
+            return jsonify(retJson)
+
+        #Step 5 store the sentence and return 200 OK
+        users.update_one({
+            "Username" : username
+        }, {
+            "$set": {
+                "Sentence" : sentence,
+                "Tokens": num_tokens - 1
+            }
+        })
+
+        retJson = { 
+            "status" : 200,
+            "msg" :  "Sentence saved"
+        }
+
+        return jsonify(retJson)
+
+class Get(Resource):
+    def post(self):
+        postedData = request.get_json()        
+
+        #Step 2 is to read the data
+        username = postedData["username"]
+        password = postedData["password"]
+
+        #Step 3 verify the username pw match
+        correct_pw = verifyPw(username, password)
+        if not correct_pw:
+            retJson = {
+                "status" : 302
+            }
+            return jsonify(retJson)
+        
+        num_tokens = countTokens(username)
+
+        if num_tokens <= 0:
+            retJson = {
+                "status" : 301
+            }
+            return jsonify(retJson)
+        
+        sentence = users.find({
+            "Username": username
+        })[0]["Sentence"]
+
+        retJson = {
+            "status": 200,
+            "sentence": sentence
+        }
+
+        return jsonify(retJson)
+
+api.add_resource(Register, '/register')
+api.add_resource(Store, '/store')
+api.add_resource(Get, '/get')
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
+
+
+
+
+
+"""
 from flask import Flask, jsonify, request
 from flask_restful  import Api, Resource 
 import os
@@ -173,5 +326,5 @@ def hello_world():
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
-
+"""
 
