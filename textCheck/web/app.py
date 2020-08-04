cfdc234d5fb70be6b8a12855d1f180b2d09072fe
Rochak Jain <rochak_r@ec.iitr.ac.in>
2020-08-04 22:44:01 +0530

    >API written for Similarity Check App

--- textCheck/web/app.py
+++ textCheck/web/app.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource
 from pymongo import MongoClient
 import os
 import bcrypt
+import spacy
 
 app = Flask(__name__)
 api = Api(app)
@@ -43,6 +44,81 @@ class Register(Resource):
             "status": 200,
             "msg": "You've successfully signed up to the API"
         }
+
+        return jsonify(retJson)
+
+def verifyPw(username, password):
+    if not UserExist(username):
+        return False
+    
+    hashed_pw = users.find({
+        "Username": username
+    })[0]["Password"]
+
+    if bcrypt.hashpw(password.encode('utf8'), hashed_pw)==hashed_pw:
+        return True
+    else:
+        return False
+
+def countTokens(username):
+    tokens = users.find({
+        "Username": username
+    })[0]["Tokens"]
+    return tokens
+
+class Detect(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+        text1 = postedData["text1"]
+        text2 = postedData["text2"]
+
+        if not UserExist(username):
+            retJson = {
+                "status": 301,
+                "msg": "Invlaid Username"
+            }
+            return jsonify(retJson)
+
+        correct_pw = verifyPw(username, password)
+
+        if not correct_pw:
+            retJson = {
+                "status": 302,
+                "msg": "Wrong Password"
+            }
+            return jsonify(retJson)
+
+        if countTokens(username) <= 0:
+            retJson = {
+                "status": 303,
+                "msg": "Not enough tokens"
+            }
+            return jsonify(retJson)
+
+        nlp = spacy.load('en_core_web_sm')
+        text1 = nlp(text1)
+        text2 = nlp(text2)
+        
+        ratio = text1.similarity(text2)
+
+        retJson = {
+            "status": 200,
+            "similarity": ratio,
+            "msg": "Score calculated"
+        }
+        
+        current_tokens = current_tokens(username)
+
+        users.update_one({
+            "Username": username,
+        },{
+            "$set":{
+                "Tokens": current_tokens - 1
+            }
+        })
 
         return jsonify(retJson)
 
